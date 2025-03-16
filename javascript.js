dea1197a6d6807b80526af6ee3679985f271b4d3
sai122005 <sairamreddy6126@gmail.com>
2025-03-16 14:07:07 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,6 @@
+document.getElementById("button").addEventListener("click", function () {
+    alert("I will kill you,if u reject meee...!");
+});
+document.getElementById("printBtn").addEventListener("click", function () {
+    window.print();
+});
